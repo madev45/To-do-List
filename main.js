f7392740df11ad834a677aceb66add9bd96bc235
M.A <m.adev9786@gmail.com>
2025-01-24 19:10:59 +0500

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,12 @@
     // Input ko LocalStorage mein save karna
     let tasks = JSON.parse(localStorage.getItem("userTask"))||[];
     
+    let obj ={
+      content : inputValue,
+      status : ""
+    }
     
-    
-    tasks.push(inputValue)
+    tasks.push(obj)
     
     
     localStorage.setItem("userTask", JSON.stringify(tasks));
@@ -25,36 +28,50 @@
 function display() {
   let saveData ; 
   
-  try {
+  
    saveData =  JSON.parse(localStorage.getItem("userTask"))||[] ;
-  } catch (e) {
-    console.log(e)
-    saveData = []
-  }
   
     let container =  document.querySelector(".container"); 
   container.innerHTML = "";
     if(saveData && saveData.length > 0) {
       
-        saveData.forEach((data) => {  
+        saveData.forEach((data, index) => {  
       
                      let div =  document.createElement("div");
         div.classList.add("new-div")
         div.innerHTML = `<p class = "output"></p>
       <div class="buttons">
+      <button class="complete">Complete</button>
       <button class="remove">Remove</button>
       </div>
     `
-    div.querySelector(".output").innerHTML = data ;
+    div.querySelector(".output").innerHTML = data.content;
     
+    if (data.status == "on") {
+      div.style.borderColor = "green"
+      div.querySelector(".complete").style.display ="none"
+      div.querySelector(".output").style.textDecoration = "line-through"
+      let span = document.createElement("span") 
+      span.classList.add("span") 
+      span.textContent = "Completed"
+      div.querySelector(".buttons").appendChild(span) 
+    }
     
+    div.querySelector(".complete").addEventListener("click",()=>{
+      
+      let taskS = JSON.parse(localStorage.getItem("userTask"))||[] ;
+      taskS[index].status = "on"
+      localStorage.setItem("userTask", JSON.stringify(taskS));
+      
+      display()
+    })
     
     
     div.querySelector(".remove").addEventListener("click",()=>{ 
      
      let taskS = JSON.parse(localStorage.getItem("userTask"))||[] ;
      
-    taskS = taskS.filter(task => task !== data);
+    taskS = taskS.filter(task => task.content !== data.content);
      
      localStorage.setItem("userTask", JSON.stringify(taskS));
      
